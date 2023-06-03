d1ed17e6396578b13d4c0e7fc2b7672144ec0d69
Pruthviraj Patil <95764252+pruthvirajp04@users.noreply.github.com>
2023-06-03 18:08:45 +0530

    NEW

--- js/summarize.js
+++ js/summarize.js
@@ -1,45 +1,44 @@
 function toggleText() {
     var textContainer = document.getElementById("textContainer");
     var uploadContainer = document.getElementById("uploadContainer");
+    var submitButton = document.getElementById("submitButton");
+    var resetButton = document.getElementById("resetButton");
   
     textContainer.classList.toggle("hidden");
     uploadContainer.classList.add("hidden");
   
     if (!textContainer.classList.contains("hidden")) {
       textContainer.classList.add("popup");
+      submitButton.classList.remove("hidden");
+      resetButton.classList.remove("hidden");
     } else {
       textContainer.classList.remove("popup");
+      submitButton.classList.add("hidden");
+      resetButton.classList.add("hidden");
     }
   }
-  
   
   function toggleUpload() {
     var textContainer = document.getElementById("textContainer");
     var uploadContainer = document.getElementById("uploadContainer");
+    var submitButton = document.getElementById("submitButton");
+    var resetButton = document.getElementById("resetButton");
   
     uploadContainer.classList.toggle("hidden");
     textContainer.classList.add("hidden");
   
     if (!uploadContainer.classList.contains("hidden")) {
       uploadContainer.classList.add("popup");
+      submitButton.classList.remove("hidden");
+      resetButton.classList.add("hidden");
     } else {
       uploadContainer.classList.remove("popup");
+      submitButton.classList.add("hidden");
+      resetButton.classList.add("hidden");
     }
   }
   
-  var uploadContainer = document.getElementById("uploadContainer");
-  uploadContainer.addEventListener("dragover", function(e) {
-    e.preventDefault();
-    this.classList.add("dragover");
-  });
+  // Hide the submit and reset buttons by default
+  document.getElementById("submitButton").classList.add("hidden");
+  document.getElementById("resetButton").classList.add("hidden");
   
-  uploadContainer.addEventListener("dragleave", function() {
-    this.classList.remove("dragover");
-  });
-  
-  uploadContainer.addEventListener("drop", function(e) {
-    e.preventDefault();
-    this.classList.remove("dragover");
-    var fileInput = document.getElementById("fileInput");
-    fileInput.files = e.dataTransfer.files;
-  });
